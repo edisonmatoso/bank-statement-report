7bc5adcccd7df6e51001836274d72f43e44ea887
Edison Matoso <ejmatoso@gmail.com>
2022-05-10 20:53:08 -0300

    chore: enabling cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3001
+
+app.use(cors())
 
 app.get('/data', (req, res) => {
   const data = require('./data.json')
